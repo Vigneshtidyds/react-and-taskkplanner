a190f6ef379a509f8723e1b2ab7bb21b4689c5bd
vigneshtidyds <vigneshwaranptidyds@gmail.com>
2025-04-24 11:06:14 +0530

    testing

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
 const year = new Date();
   return (
-    <div>copy &copy; {year.getFullYear()} </div>
+    <div>Copyrights &copy; {year.getFullYear()} </div>
   )
 }
 
