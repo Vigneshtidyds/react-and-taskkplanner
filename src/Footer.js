2072696191a77b9334199684b7cc207dffe103e7
vigneshtidyds <vigneshwaranptidyds@gmail.com>
2025-04-24 11:34:08 +0530

    update footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
 const year = new Date();
   return (
-    <div>copy &copy; {year.getFullYear()} </div>
+    <div>helloworld &copy; {year.getFullYear()} </div>
   )
 }
 
